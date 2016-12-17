959c51f0ff582f068fc8d7f065285ec4dd0076a0
kadnox <yvann.josso@gmail.com>
2016-12-17 16:34:25 +0100

    Pour Jojo

--- src/main/java/org/opencompare/analyse/AnalyseImpl.java
+++ src/main/java/org/opencompare/analyse/AnalyseImpl.java
@@ -29,7 +29,6 @@ public class AnalyseImpl implements Analyse {
         Map<String,Integer> mapOccFeature = new HashMap<>();
         ArrayList<String> laListe = (ArrayList<String>) getTableCompare().get(new String(feature));
         String value;
-        int occ;
         Iterator<String> it = laListe.iterator();
         while(it.hasNext()){
             value = it.next();
@@ -61,5 +60,48 @@ public class AnalyseImpl implements Analyse {
             isNumber = value.matches("[0-9]*");
         }
         return isNumber;
+    }
+
+    public boolean isString(String feature){
+        ArrayList<String> laListe = (ArrayList<String>) getTableCompare().get(new String(feature));
+        System.currentTimeMillis();
+        Iterator<String> it = laListe.iterator();
+        String value;
+        boolean isString = true;
+        while (it.hasNext() && isString){
+            value = it.next();
+            isString = value.matches("[0-9]{4}");
+        }
+        return isString;
+    }
+
+    public boolean isDateUK(String feature){
+        ArrayList<String> laListe = (ArrayList<String>) getTableCompare().get(new String(feature));
+        Iterator<String> it = laListe.iterator();
+        String value;
+        boolean isDate = true;
+        while (it.hasNext() && isDate){
+            value = it.next();
+            isDate = value.matches("[0-9]{4}-[0-9]{2}-[0-9]{2}") || value.matches("[0-9]{4}-[0-9]{2}");
+        }
+        return isDate;
+    }
+
+    public boolean correctYear(String year){
+        Calendar cal = Calendar.getInstance();
+        cal.getWeekYear();
+        return year.matches("[0-9]{4} || [0-9]{2}");
+    }
+
+    public boolean isPercent(String feature){
+        ArrayList<String> laListe = (ArrayList<String>) getTableCompare().get(new String(feature));
+        Iterator<String> it = laListe.iterator();
+        String value;
+        boolean isPercent = true;
+        while (it.hasNext() && isPercent){
+            value = it.next();
+            isPercent = value.matches("[0-9]{1,3}%");
+        }
+        return isPercent;
     }
 }
