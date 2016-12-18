7a5ad5e2e88db173b3930e670c67513e20804118
htsa <heditheo1@gmail.com>
2016-12-18 11:59:24 +0100

    premier graphique en camembert

--- resources/templates/js/chart.js
+++ resources/templates/js/chart.js
@@ -12,8 +12,60 @@ function noProductAdded() {
 }
 
 function generatePie() {
+    var str = $('#select_feature option:selected').text();
+    $("p").css("color","red");
+    $("#test").append(  str +"</br>" );
+    var person = new Object();
+
+    $("#list_products_added .present").each(function(){
+            var res = $(this).data(str);
+            var mot = $(this).text();
+
+        if ( res in person) {
+            person[res] = person[res]+1
+          //  $("#test").append(  "on creer pas " + res + "qui a la valeur " + person[res] + " </br>" );
+        } else {
+            person[res] = 1
+           // $("#test").append(  "on creer " + res + "qui a la valeur " + person[res]+ " </br>" );
+        }
+
+
+    })
+    var couleur =["#878BB6","#4ACAB4","#FF8153","#FFEA88"]
+    var num_couleur = 0
+    var labels = []
+    var backgroundColor = []
+    var donne = []
+/*var pieData = []
+    for (var i in person){
+        $("#test").append( "il y a " + i + ": "+ person[i] + " </br>" );
+        var tmp = {
+            value : person[i],
+            color : couleur[num_couleur]
+        }
+        pieData.push(tmp)
+        num_couleur = num_couleur + 1
+    }*/
+
+
+    for (var i in person){
+        $("#test").append( "il y a " + i + ": "+ person[i] + " </br>" );
+       labels.push(i)
+        backgroundColor.push(couleur[num_couleur])
+        donne.push(person[i])
+        num_couleur = num_couleur + 1
+    }
+
+
+
+
+
+
+
+
+
     if(checkProductList()) {
-        var pieData = [
+       /* var pies = [
             {
                 value: 20,
                 color: "#878BB6"
@@ -30,7 +82,11 @@ function generatePie() {
                 value: 30,
                 color: "#FFEA88"
             }
-        ];
+        ];*/
+
+
+
+
 
         //Partie de redimensionnement obligatoire pour le responsive
         var parent = document.getElementById('canvas_container');
@@ -39,10 +95,30 @@ function generatePie() {
         var canvas = document.getElementById("charts");
         var ctx = canvas.getContext("2d");
 
+
         canvas.width = parent.offsetWidth;
         canvas.height = parent.offsetHeight;
 
-        new Chart(ctx).Pie(pieData);
+        var mycharto = new Chart(ctx,{
+            type : "pie",
+            data : {
+                labels : labels,
+                datasets : [
+                    {
+                        backgroundColor: backgroundColor,
+                        data : donne
+                    }
+                ]
+            }
+            }
+            );
+
+       // new Chart(ctx).Pie(pieData);
+
+
+
+
+
     } else {
         noProductAdded();
     }
--- src/test/java/org/opencompare/GettingStartedTest.java
+++ src/test/java/org/opencompare/GettingStartedTest.java
@@ -22,8 +22,8 @@ public class GettingStartedTest {
     public void testGettingStarted() throws IOException {
 
         // Define a file representing a PCM to load
-        File pcmFile = new File("pcms/example.pcm");
-
+        //File pcmFile = new File("pcms/Comparison_of_ADC_software_5.pcm");
+        File pcmFile = new File("pcms/Comparison_between_Esperanto_and_Ido_2.pcm");
 
         // Create a loader that can handle the file format
         PCMLoader loader = new KMFJSONLoader();
@@ -122,22 +122,29 @@ public class GettingStartedTest {
             Object [] liste_feature=  ensemble_feature.toArray();
 
             for(int i =0; i<liste_feature.length;i++){
-                System.out.println("la feature numero " + i + " est " + liste_feature[i]);
+               // System.out.println("la feature numero " + i + " est " + liste_feature[i]);
             }
 
 
 
-
+            System.out.println("bla bla \n \n" +liste_feature[0] + "\n \n");
+            String pb= (String) liste_feature[0];
+            for( int i =0 ; i<pb.length(); i++){
+                char pbsuite = pb.charAt(i) ;
+                int asciipb = (int) pbsuite;
+                System.out.println("le " + i +" eme caractere est " + pbsuite + " voila avec comme code ascii " + asciipb);
+            }
             String newline = System.getProperty("line.separator");
             int asciiInsecable= 160;
             char insecable = (char) asciiInsecable;
             int asciiUnderscore = 95;
             char underscore = (char)asciiUnderscore;
+            int asciiRetourLigne = 10;
+            char charRetourLigne = (char) asciiRetourLigne;
 
 
-
-            FileWriter writer = new FileWriter("src/test/files/resultat.html");
-            FileWriter writerjson = new FileWriter("src/test/files/resultat.json");
+            FileWriter writer = new FileWriter("templates/resultat.html");
+            FileWriter writerjson = new FileWriter("templates/resultat.json");
             writerjson.write("{");
 
 
@@ -150,10 +157,11 @@ public class GettingStartedTest {
                     "\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>\n" +
                     "\t\t<link href=\"http://fonts.googleapis.com/icon?family=Material+Icons\" rel=\"stylesheet\">\n" +
                     "  \t\t<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css\">\n" +
-                    "\t\t<link rel=\"stylesheet\" href=\"style.css\">\n" +
+                    "\t\t<link rel=\"stylesheet\" href=\"css/style.css\">\n" +
                     "\t\t\n" +
-                    "\t\t<script type=\"text/javascript\" src=\"userConfig.js\"></script>\n" +
-                    "\t\t<script type=\"text/javascript\" src=\"chart.js\"></script>\n" +
+                    "\t\t<script type=\"text/javascript\" src=\"js/userConfig.js\"></script>\n" +
+                    "\t\t<script type=\"text/javascript\" src=\"js/chart.js\"></script>\n" +
+                  "  <script src=\"https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.min.js\"></script> " +
                     "\n" +
                     "\t\t<title>PCM Example </title>\n" +
                     "\t</head>\n" +
@@ -163,7 +171,9 @@ public class GettingStartedTest {
                     "\t\t\t\t<select id=\"select_feature\" onChange='enableChartIcons()'>\n" +
                     "\t\t\t\t\t<option value=\"0\" disabled selected>Select feature</option>");
             for(int i =1; i<liste_feature.length+1;i++){
-                writer.write("<option value="+ i+"> "+ liste_feature[i-1] + "</option>" + newline);
+                String tmp =(String) liste_feature[i-1];
+              tmp =   tmp.toLowerCase().replace("'","_").replace('"','_').replace(insecable,underscore).replace(" ","_").replace("/","_par_").replace(charRetourLigne,underscore);
+                writer.write("<option value="+i+">"+tmp+"</option>" + newline);
             }
 
             writer.write("</select>\n" +
@@ -190,29 +200,29 @@ public class GettingStartedTest {
                 writer.write("<li class=\"collection-item\">");
                 Map temporaire =(Map)ht.get(liste_produit[i]); //
                 writerjson.write("\""+liste_produit[i]+"\" : \n {");
-                String ligne_html = "<span class='product_name' ";
+                String ligne_html = "<span class='product_name present' ";
 
                 for (int j =0 ; j<liste_feature.length; j++){
                     String nom_feature= (String)liste_feature[j];
                     String data_tmp = (String)temporaire.get(liste_feature[j]);
-                    data_tmp = data_tmp.replace("'"," ").replace('"',' ');
+                    data_tmp = data_tmp.replace("'"," ").replace('"',' ').replace(charRetourLigne,underscore);
                     if(j<liste_feature.length-1){
                         writerjson.write("\""+ nom_feature +"\":\"" +data_tmp + "\", \n");
                     }
                     else{
                         writerjson.write("\""+ nom_feature +"\":" +data_tmp + "\n ");
                     }
-                    nom_feature= nom_feature.toLowerCase().replace("'","_").replace('"','_').replace(insecable,underscore).replace(" ","_").replace("/","_par_");
+                    nom_feature= nom_feature.toLowerCase().replace("'","_").replace('"','_').replace(insecable,underscore).replace(" ","_").replace("/","_par_").replace(charRetourLigne,underscore);
 
 
 
-                    data_tmp = data_tmp.replace("'","_").replace('"','_').replace(insecable,underscore).replace(" ","_");
+                    data_tmp = data_tmp.replace("'","_").replace('"','_').replace(insecable,underscore).replace(" ","_").replace(charRetourLigne,underscore);
                     ligne_html = ligne_html + "data-" + nom_feature + "='"+ data_tmp + "' ";
                 }
 
 
                 String  produit = liste_produit[i];
-                produit = produit.replace("'","_").replace('"','_').replace(insecable,underscore).replace(" ","_");
+                produit = produit.replace("'","_").replace('"','_').replace(insecable,underscore).replace(" ","_").replace(charRetourLigne,underscore);
 
 
                 writer.write(ligne_html +">"+ liste_produit[i] +"</span>"+ newline );
@@ -265,39 +275,42 @@ public class GettingStartedTest {
                     "\n" +
                     "\t\t\t\t<div class=\"col s12 center chart_icons hide\">\n" +
                     "\t\t\t\t\t<a class=\"chart_icon\" onClick=\"generatePie()\">\n" +
-                    "\t\t\t\t\t\t<img class=\"responsive-img\" src=\"../../../images/pie_chart.png\" alt=\"Pie chart\">\n" +
+                    "\t\t\t\t\t\t<img class=\"responsive-img\" src=\"../images/pie_chart.png\" alt=\"Pie chart\">\n" +
                     "\t\t\t\t\t</a>\n" +
                     "\n" +
                     "\t\t\t\t\t<a class=\"chart_icon\" onClick=\"generateBar()\">\n" +
-                    "\t\t\t\t\t\t<img class=\"responsive-img\" src=\"../../../images/bar_chart.png\" alt=\"Bar chart\">\n" +
+                    "\t\t\t\t\t\t<img class=\"responsive-img\" src=\"../images/bar_chart.png\" alt=\"Bar chart\">\n" +
                     "\t\t\t\t\t</a>\n" +
                     "\n" +
                     "\t\t\t\t\t<a class=\"chart_icon\" onClick=\"generateRadar()\">\n" +
-                    "\t\t\t\t\t\t<img class=\"responsive-img\" src=\"../../../images/radar_chart.png\" alt=\"Radar chart\">\n" +
+                    "\t\t\t\t\t\t<img class=\"responsive-img\" src=\"../images/radar_chart.png\" alt=\"Radar chart\">\n" +
                     "\t\t\t\t\t</a>\n" +
                     "\t\t\t\t</div>\n" +
                     "\t\t\t</div>\n" +
                     "\t\t</div>\n" +
                     "\n" +
                     "\t\t<div class=\"row\">\n" +
-                    "\t\t\t<div class=\"divider\"></div>\n" +
+
                     "\n" +
-                    "\t\t\t<div class=\"col s12\">\n" +
-                    "\t\t\t\t<canvas id=\"charts\"></canvas>\n" +
+                    "\t\t\t<div id=\"canvas_container\" class=\"col s12\">\n" +
+                    "\t\t\t\t<canvas id=\"charts\"> </canvas>\n" +
                     "\t\t\t</div>\n" +
                     "\t\t</div>\n" +
-                    "\t</body>\n" +
+
                     "\n" +
                     "\t<script type=\"text/javascript\" src=\"https://code.jquery.com/jquery-2.1.1.min.js\"></script>\n" +
                     "  \t<script src=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js\"></script>\n" +
-                    "  \t<script src=\"https://cdnjs.cloudflare.com/ajax/libs/Chart.js/0.2.0/Chart.min.js\" type=\"text/javascript\"></script>\n" +
+
                     "\n" +
+                    "<script src=\"js/htsa.js\"> </script>"+
                     "\t<script type=\"text/javascript\">\n" +
                     "\t\t$(document).ready(function() {\n" +
                     "\t\t\t$('select').material_select();\n" +
                     "\t\t});\n" +
                     "\t</script>\n" +
+                    "\t</body>\n" +
                     "</html>");
+
             writerjson.write("}");
             writer.close();
             writerjson.close();
